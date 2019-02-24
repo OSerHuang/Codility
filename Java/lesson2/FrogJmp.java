1b0a0baa768ffafd88fdb14db74470880675c5f1
OSer Huang <oser.huang@gmail.com>
2019-02-24 22:44:52 +0100

    add FrogJmp.java

---
+++ Java/lesson2/FrogJmp.java
@@ -0,0 +1,12 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int X, int Y, int D) {
+        // write your code in Java SE 8
+        return (Y - X) / D + ((Y - X) % D > 0 ? 1: 0); 
+    }
+}
