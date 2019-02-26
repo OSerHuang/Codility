00312b1717c4e31e08c2d9a6e0486a4718f42e2e
OSer Huang <oser.huang@gmail.com>
2019-02-26 13:21:20 +0100

    Add PermCheck.java

---
+++ Java/lesson 04/PermCheck.java
@@ -0,0 +1,17 @@
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int val: A) {
+            if (val > A.length || set.contains(val)) return 0;
+            set.add(val);
+        }
+        return 1;
+    }
+}
