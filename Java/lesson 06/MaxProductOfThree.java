5839ddb361749a39fe0c23afa07e7df4530a266b
OSer Huang <oser.huang@gmail.com>
2019-03-02 21:53:20 +0100

    Add MaxProductOfThree.java

---
+++ Java/lesson 06/MaxProductOfThree.java
@@ -0,0 +1,14 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        Arrays.sort(A);
+        int len = A.length;
+        return A[len - 1] * A[len - 2] * A[len - 3] > A[0] * A[1] * A[len - 1]? A[len - 1] * A[len - 2] * A[len - 3]: A[0] * A[1] * A[len - 1];
+    }
+}
